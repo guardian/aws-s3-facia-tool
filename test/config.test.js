ff1197326e3c6306c60b6d9e10c5899e6f6b5a93
Fabio Crisci <fabio.crisci@guardian.co.uk>
2015-05-06 10:27:36 +0100

    Query language for configuration

--- test/config.test.js
+++ test/config.test.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var json = require('./samples/config.json');
+var jsonFind = require('./samples/configFind.json');
 
 describe('config', function () {
 	var Config = require('../lib/config');
@@ -21,5 +22,50 @@ describe('config', function () {
 			'displayName': 'External links',
 			'type': 'nav/list'
 		});
+	});
+
+	it('finds a front', function () {
+		var instance = new Config(jsonFind);
+		var allResults = Object.keys(jsonFind.fronts).map(function (key) {
+			return jsonFind.fronts[key];
+		});
+
+		expect(instance.fronts.find()).to.deep.equal(allResults);
+		expect(instance.fronts.find({})).to.deep.equal(allResults);
+		expect(instance.fronts.find({
+			a: 'text'
+		})).to.deep.equal([jsonFind.fronts.one, jsonFind.fronts.five]);
+		expect(instance.fronts.find({
+			a: { $in: ['text', 'string'] }
+		})).to.deep.equal([jsonFind.fronts.one, jsonFind.fronts.four, jsonFind.fronts.five]);
+		expect(instance.fronts.find({
+			a: 'text',
+			c: 20
+		})).to.deep.equal([jsonFind.fronts.five]);
+		expect(instance.fronts.find({
+			$or: [ { a: 'string' }, { c: 20 } ]
+		})).to.deep.equal([jsonFind.fronts.four, jsonFind.fronts.five]);
+		// This doesn't work on sift
+		// expect(instance.fronts.find({
+		// 	'b': ['c1', 'c2', 'c3']
+		// })).to.deep.equal([jsonFind.fronts.three]);
+		expect(instance.fronts.find({
+			'b': 'c1'
+		})).to.deep.equal([jsonFind.fronts.three, jsonFind.fronts.six]);
+		// This doesn't work on sift
+		// expect(instance.fronts.find({
+		// 	'b.0': 'c1'
+		// })).to.deep.equal([jsonFind.fronts.three]);
+		expect(instance.fronts.find({
+			'a': { $regex: /text/ }
+		})).to.deep.equal([jsonFind.fronts.one, jsonFind.fronts.two, jsonFind.fronts.five]);
+	});
+
+	it('finds a collection', function () {
+		var instance = new Config(jsonFind);
+
+		expect(instance.collections.find({
+			a: 'some'
+		})).to.deep.equal([jsonFind.collections.one]);
 	});
 });
