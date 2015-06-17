1a632d330c27fe6c67716c6f123fab8e8fa5f017
Fabio Crisci <fabio.crisci@guardian.co.uk>
2015-06-17 16:33:50 +0100

    Get the fronts containing a given collection

--- test/collection.test.js
+++ test/collection.test.js
@@ -37,4 +37,42 @@ describe('collection', function () {
 			'live__two': { id: 'two' }
 		});
 	});
+
+	it('finds the fronts containing that collection', function () {
+		var config = {
+			fronts: {
+				'one': {
+					collections: ['a', 'b', 'c']
+				},
+				'two': {
+					collections: ['b']
+				},
+				'three': {
+					collections: ['b', 'c', 'd']
+				},
+				'four': {
+					collections: []
+				}
+			},
+			collections: {}
+		}, instance, fronts;
+
+		instance = new Collection('a', config);
+		fronts = instance.fronts();
+		expect(fronts.length).to.equal(1);
+		expect(fronts[0].id).to.equal('one');
+		expect(fronts[0].allCollections()).to.deep.equal(['a', 'b', 'c']);
+
+		instance = new Collection('b', config);
+		fronts = instance.fronts();
+		expect(fronts.length).to.equal(3);
+		expect(fronts[0].id).to.equal('one');
+		expect(fronts[1].id).to.equal('two');
+		expect(fronts[2].id).to.equal('three');
+		expect(fronts[1].allCollections()).to.deep.equal(['b']);
+
+		instance = new Collection('e', config);
+		fronts = instance.fronts();
+		expect(fronts.length).to.equal(0);
+	});
 });
