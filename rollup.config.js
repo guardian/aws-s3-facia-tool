8f15ec7485c5e005097cde4e466f011d980d5581
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-04-22 16:24:51 +0100

    rollup to es5

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
+import babel from 'rollup-plugin-babel';
 
 const nodeResolveConfig = {};
 
@@ -11,6 +12,7 @@ export default {
 	external: ['aws-sdk'],
 	plugins: [
 		json(),
+		babel(),
 		nodeResolve(nodeResolveConfig),
 		commonjs()
 	]
